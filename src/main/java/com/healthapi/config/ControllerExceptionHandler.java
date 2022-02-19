6a1ff5e35bce960ed011a7ec9424f478734e54ef
erashape <erashape@gmail.com>
2022-02-19 10:03:26 +0900

    1. Response 객체 변경

--- src/main/java/com/healthapi/config/ControllerExceptionHandler.java
+++ src/main/java/com/healthapi/config/ControllerExceptionHandler.java
@@ -1,12 +1,10 @@
 package com.healthapi.config;
 
 import com.healthapi.response.ApiException;
-import com.healthapi.response.ResponseCode;
 import com.healthapi.response.ApiResponse;
-import com.healthapi.service.ResponseService;
+import com.healthapi.response.ResponseCode;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -14,25 +12,19 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 @RequiredArgsConstructor
 public class ControllerExceptionHandler {
-    private final ResponseService responseService;
-
     @ExceptionHandler(Exception.class)
     protected ApiResponse<ResponseCode> exception(Exception e) {
         log.error("Exception", e);
         log.warn("Exception message:[{}]", e.getMessage());
 
-        return responseService.responseResult(ResponseCode.SERVER_ERROR);
+        return ApiResponse.result(ResponseCode.SERVER_ERROR);
     }
 
     @ExceptionHandler(ApiException.class)
-    protected ApiResponse<ResponseCode> customException(ApiException e) {
+    protected ApiResponse<ResponseCode> apiException(ApiException e) {
         log.info("Response Message:[{}]", e.getMessage());
         log.warn("Response message:[{}]", e.getMessage());
 
-        if(StringUtils.hasText(e.getMessage())) {
-            return responseService.responseResult(ResponseCode.getResponse(e.getCode()), e.getMessage());
-        }
-
-        return responseService.responseResult(ResponseCode.getResponse(e.getCode()));
+        return ApiResponse.result(e.getResponseCode());
     }
 }
