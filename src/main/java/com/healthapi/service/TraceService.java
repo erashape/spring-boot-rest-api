5beb794b6fc3dffcd8d33edbf84638f1dcaf4f99
erashape <erashape@gmail.com>
2022-03-20 07:47:59 +0900

    sleuth 추가

---
+++ src/main/java/com/healthapi/service/TraceService.java
@@ -0,0 +1,23 @@
+package com.healthapi.service;
+
+import brave.Tracer;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class TraceService {
+    private final Tracer tracer;
+
+    public String spanId() {
+        var span = tracer.currentSpan();
+        return span.context().spanIdString();
+    }
+
+    public String traceId() {
+        var span = tracer.currentSpan();
+        return span.context().traceIdString();
+    }
+}
